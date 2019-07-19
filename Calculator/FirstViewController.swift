4d8b03533ec0568dcee9b6cd0333020b946e26f3
Ivan Demchenko <vanuademchenko@gmail.com>
2019-07-19 15:38:23 +0300

    Initial Commit

---
+++ Calculator/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Calculator
+//
+//  Created by Иван Демченко on 2019-07-19.
+//  Copyright © 2019 Ivan Demchenko. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Calculator/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Calculator
+//
+//  Created by Иван Демченко on 2019-07-19.
+//  Copyright © 2019 Ivan Demchenko. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
